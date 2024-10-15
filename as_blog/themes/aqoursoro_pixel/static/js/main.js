d0076d52ffc64d3a819981b6aa6d420a6a1f0204
AqourSoro <971755995@qq.com>
2024-10-15 19:07:06 +0800

    Fix Multilingual settings

--- as_blog/themes/aqoursoro_pixel/static/js/main.js
+++ as_blog/themes/aqoursoro_pixel/static/js/main.js
@@ -1,18 +1,6 @@
-// 切换语言的功能
-function toggleLanguage() {
-    const currentUrl = window.location.href;
-    const isEnglish = !currentUrl.includes('/cn/');  // 检查是否为默认语言
 
-    const newUrl = isEnglish
-        ? currentUrl.replace(window.location.origin, window.location.origin + '/cn')  // 切换到中文
-        : currentUrl.replace('/cn', '');  // 切换回默认语言
-
-    window.location.href = newUrl;  // 重定向到新的 URL
-}
-
-
+// 动态加载页面内容
 function loadContent(section, url) {
-    // 不改变当前页面内容，直到新内容加载成功
     const rightColumn = document.querySelector('.right-column');  // 页面内容区
 
     fetch(url)
@@ -32,26 +20,18 @@ function loadContent(section, url) {
         })
         .catch(error => {
             console.error('Error loading content:', error);
-            // 如果加载失败，保持页面不变，什么都不做
         });
 
     // 更新 URL
     history.pushState(null, null, url);
 }
 
-
-
-// 初始化语言切换按钮状态
+// 初始化页面语言切换按钮
 document.addEventListener('DOMContentLoaded', () => {
-
-    // 根据当前 URL 加载正确的页面内容（初始化）
-    loadContent('page', window.location.href);
-
-    
+    loadContent('page', window.location.href);  // 加载当前URL内容
 });
 
 // 监听浏览器前进/后退按钮的点击事件
 window.addEventListener('popstate', function(event) {
-    // 当点击后退按钮时重新加载正确的页面内容
     loadContent('page', window.location.href);
 });
